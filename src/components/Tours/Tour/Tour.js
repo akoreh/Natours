94d66d339ee23a96be4923d4dee491d5d35f75c9
Koreh Andrei <koreh.andrei20@gmail.com>
2018-08-13 20:07:33 +0300

    Added tours section

---
+++ src/components/Tours/Tour/Tour.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import './Tour.css';
+import Button from '../../UI/Button/Button';
+
+const tour = (props) => {
+    return (
+        <div className="card">
+            <div className="card__side card__side--front">
+                <div className={`card__image card__image--${props.index+1}`}/>
+                <h4 className="card__heading">
+                    <span className={`card__heading-span card__heading-span--${props.index+1}`}>
+                        {props.title}
+                    </span>
+                </h4>
+                <div className="card__details">
+                    <ul>
+                        {
+                            props.details.map(detail => (
+                                <li>{detail}</li>
+                            ))
+                        }
+                    </ul>
+                </div>
+            </div>
+            <div className={`card__side card__side--back card__side--back-${props.index+1}`}>
+                <div className="card__cta">
+                    <div className="card__price-box">
+                        <p className="card__price-only">Only</p>
+                        <p className="card__price-value">${props.price}</p>
+                    </div>
+                    <Button color="white">Book now!</Button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default tour;
---
+++ src/components/Tours/Tours.js
@@ -0,0 +1,67 @@
+import React from 'react';
+
+import './Tours.css';
+import Tour from './Tour/Tour';
+import Button from '../UI/Button/Button';
+
+const tours = () => {
+    const tours = [
+        {
+            title: 'The sea explorer',
+            price: 267,
+            details: [
+                '3 day tour',
+                'Up to 30 people',
+                '2 tour guides',
+                'Sleep in cozy hostels',
+                'Difficulty: very easy'
+            ]
+        },
+        {
+            title: 'The forest hiker',
+            price: 454,
+            details: [
+                '7 day tour',
+                'Up to 40 people',
+                '6 tour guides',
+                'Sleep in provided tents',
+                'Difficulty: medium'
+            ]
+        },
+        {
+            title: 'The snow adventurer',
+            price: 786,
+            details: [
+                '5 day tour',
+                'Up to 15 people',
+                '3 tour guides',
+                'Sleep in provided tents',
+                'Difficulty: hard'
+            ]
+        }
+    ]
+
+
+    return (
+        <section className="section-tours">
+            <div className="u-center-text u-margin-bottom-large">
+                <h2 className="heading-secondary">Most Popular Tours</h2>
+            </div>
+            <div className="row">
+                    {
+                        tours.map((tour, index) => (
+                            <div className="col-1-of-3">
+                                <Tour {...tour}  index={index}/>
+                            </div>
+                        ))
+                    }
+            </div>
+
+            <div className="u-center-text u-margin-top-large">
+                <Button color="green">Discover all tours</Button>
+            </div>
+        </section>
+    );
+}
+
+export default tours;
