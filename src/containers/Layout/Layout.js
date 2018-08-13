2a108182f2a5f33c8ee35537be6cfeba16f77283
Koreh Andrei <koreh.andrei20@gmail.com>
2018-08-13 20:07:50 +0300

    Added tours and features to layout

--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -3,12 +3,16 @@ import React from 'react';
 import Wrapper from '../../hoc/Wrapper/Wrapper';
 import Header from '../../components/Header/Header';
 import About from '../../components/About/About';
+import Features from '../../components/Features/Features';
+import Tours from '../../components/Tours/Tours';
 
 const layout = () => (
     <Wrapper>
         <Header />
         <main>
             <About />
+            <Features />
+            <Tours />
         </main>
     </Wrapper>
 );
