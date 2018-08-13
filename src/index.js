dba1eb7957f835ca65ad52e831f66df088b9f291
Koreh Andrei <koreh.andrei20@gmail.com>
2018-08-13 20:07:11 +0300

    Added linea basic icon font

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './assets/fonts/linea-basic.css'; // Linea basic icon font
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
