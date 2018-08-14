4482f614b24ef6780fea2f8f1022e9140041bae5
Koreh Andrei <koreh.andrei20@gmail.com>
2018-08-14 15:48:47 +0300

    Added radio button component

---
+++ src/components/UI/Radio/Radio.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './Radio.css';
+import Wrapper from '../../../hoc/Wrapper/Wrapper';
+
+const radio = (props) => {
+    return (
+        <Wrapper>
+            <input name={props.name} type="radio" id={props.value} className="radio__input" />
+            <label for={props.value} className="radio__label">
+                <span className="radio__button" />
+                {props.label}
+            </label>
+        </Wrapper>
+    );
+}
+
+export default radio;
