25812a2f8113ad8e4a53b4ff3d81fbb93b27b651
Koreh Andrei <koreh.andrei20@gmail.com>
2018-08-14 10:08:51 +0300

    Story component content and style

--- src/components/Stories/Story/Story.js
+++ src/components/Stories/Story/Story.js
@@ -2,9 +2,26 @@ import React from 'react';
 
 import './Story.css';
 
+
 const story = (props) => {
     return (
-        <h1> Story </h1>
+        <div className="story">
+            <figure className="story__shape">
+                <img src={props.image} alt="Person on a tour" className="story__img"/>
+                <figcaption className="story__caption">
+                    {props.caption}
+                </figcaption>
+            </figure>
+            <div className="story__text">
+                <h3 className="heading__tertiary u-margin-bottom-small">
+                    {props.title}
+
+                </h3>
+                <p>
+                    {props.text}
+                </p>
+            </div>
+        </div>
     );
 }
 
